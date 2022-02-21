2af896ac862a83ce1dd14d6f8ad795acedfc9640
Morteza <44939554+Mozart4242@users.noreply.github.com>
2022-02-21 13:40:46 +0330

    Create es_health.py

---
+++ Automation/es_health.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from distutils.command.clean import clean
+from elasticsearch import Elasticsearch
+import requests
+
+es = Elasticsearch(
+    ['10.132.160.222'],
+#    http_auth=('ciso', 'qw12QW12'),
+    scheme="http",
+    port=9200,
+#    verify_certs=False
+)
+
+resp = es.info(pretty=True)
+#print(resp)
+health = es.cluster.health()
+
+## a very simple code to show us the cluster health
+print(f"Your node {resp['name']} in the {resp['cluster_name']} is {health['status']}")
